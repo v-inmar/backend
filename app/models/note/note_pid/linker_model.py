7ec7608a4cfc446412e704492db125dbd9fc31df
Vinmar <vinmar.tipay@outlook.com>
2021-05-22 22:28:30 +0000

    Created NotePIDLinkerModel

---
+++ app/models/note/note_pid/linker_model.py
@@ -0,0 +1,19 @@
+import datetime
+from app import db
+
+
+class NotePIDLinkerModel(db.Model):
+    """
+    Creates a data access object for NotePIDLinkerModel
+    This links the NoteModel to the NotePIDModel, thus giving every note a unique pid
+    """
+    __tablename__ = 'note_pid_linker_model'
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    note_id = db.Column(db.BigInteger, db.ForeignKey("note_model.id"))
+    note_pid_id = db.Column(db.BigInteger, db.ForeignKey("note_pid_model.id"))
+    timestamp = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, **kwargs):
+        self.note_id = kwargs['note_id']
+        self.note_pid_id = kwargs['note_pid_id']
+        self.timestamp = datetime.datetime.utcnow()
