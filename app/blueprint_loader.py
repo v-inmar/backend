f6959154ab6df67af9e49d1f27a23b673f06dbaf
Vinmar <vinmar.tipay@outlook.com>
2021-05-24 20:51:49 +0000

    Removed the presence of item, not needed anymore

--- app/blueprint_loader.py
+++ app/blueprint_loader.py
@@ -1,10 +1,6 @@
 # from app import app
 from app.index import app
 
-# Register the items blueprint into the app
-from app.api.items.blueprint import items_bp
-app.register_blueprint(items_bp)
-
 # Register the note blueprint into the app
 from app.api.notes.blueprint import notes_bp
 app.register_blueprint(notes_bp)
